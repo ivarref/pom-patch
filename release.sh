bafc8f8cd98b44b88d660c4234462a810a0d236c
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-24 12:58:22 +0100

    Prepare release

--- release.sh
+++ release.sh
@@ -10,6 +10,15 @@ let "NEXT_PATCH=COMMIT_COUNT+1"
 
 clojure -X ivarref.pom-patch/clojars-repo-only!
 VERSION=$(clojure -X ivarref.pom-patch/set-patch-version! :patch "$NEXT_PATCH")
-
-echo "version is $VERSION"
 clojure -X ivarref.pom-patch/update-tag!
+
+git add pom.xml
+git commit -m "Release $VERSION"
+git tag -a v$NEW_VERSION -m "Release v$VERSION"
+git push --follow-tags
+
+clojure -M:deploy
+
+echo "Released $VERSION"
+
+rm *.pom.asc
